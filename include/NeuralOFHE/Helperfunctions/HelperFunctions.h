ee5fa7c4f1df426d163de08a8b4fd4c7ba3d2941
Linus Henke <linus.henke@mci.edu>
2023-06-20 17:25:56 +0200

    Started commenting.

--- include/NeuralOFHE/Helperfunctions/HelperFunctions.h
+++ include/NeuralOFHE/Helperfunctions/HelperFunctions.h
@@ -5,12 +5,32 @@
 #include "../Operators/Operator.h"
 
 
+/***
+ * Function that sets the context object for the entire crypto environment.
+ *
+ * @param context Context object that should be used
+ */
 void SetContext(CryptoContext<DCRTPoly> context);
 
 
+/***
+ * Function that generates a vector of ints which is filled with the indices needed for matrix multiplication according
+ * to the contexts batchSize
+ *
+ * @param batchSize batchSize of the context
+ * @return Vector of integers containing all needed rotation indices needed for matrix multiplication
+ */
 std::vector<int> GetRotations (uint32_t batchSize);
 
 
+/***
+ * Function that creates and returns shared pointer pointing to an Operator inherited object
+ *
+ * @tparam Operation Class of the Operation
+ * @tparam Args
+ * @param args Arguments of the classes constructor
+ * @return Pointer to the Operation
+ */
 template <typename Operation, typename... Args>
 extern std::shared_ptr<Operator> AddOperation (Args... args) {
     std::shared_ptr<Operator> ptr(std::make_shared<Operation>(args...));
--- include/NeuralOFHE/Operators/Activation.h
+++ include/NeuralOFHE/Operators/Activation.h
@@ -3,10 +3,29 @@
 
 #include "Operator.h"
 
+
+/***
+ * Base class for activation functions
+ */
 class ActivationFunction : public Operator{
 public:
+    /***
+     * Constructor of an ActivationFunction object.
+     *
+     * @param Min
+     * @param Max
+     * @param polyDeg
+     * @param objCounter
+     * @param name
+     */
     ActivationFunction(double Min, double Max, uint32_t polyDeg, uint32_t& objCounter, std::string name);
 
+    /***
+     * Applying the activation function to the input.
+     *
+     * @param x Input
+     * @return Output
+     */
     Ciphertext<DCRTPoly> forward(Ciphertext<DCRTPoly> x) override;
 
 protected:
